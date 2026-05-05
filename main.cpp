b366d0f82dbf50aa4c2b0849e6022ec3a21da825
A Student <student@nowhere.edu>
2026-05-05 00:39:48 +0000

    May 4, 2026, 5:39 PM

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ int main()
     }
 
     balance = balance - actualpayment;
-    cout << month << "\t" << interest << "\t" << 
+    cout << month << "\t" << interest << "\t" << actualpayment 
   }
 
 }
